2321db553b8eb02e586d3c706bc599eeef5577cb
Phenix Rizen <phenixrizen@gmail.com>
2018-09-02 19:21:47 -0700

    changed paths for test

--- internal/suggest/formatters_test.go
+++ internal/suggest/formatters_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/mdempsky/gocode/internal/suggest"
+	"github.com/phenixrizen/gocode/internal/suggest"
 )
 
 func TestFormatters(t *testing.T) {
--- internal/suggest/suggest_test.go
+++ internal/suggest/suggest_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/mdempsky/gocode/internal/suggest"
+	"github.com/phenixrizen/gocode/internal/suggest"
 )
 
 func TestRegress(t *testing.T) {
